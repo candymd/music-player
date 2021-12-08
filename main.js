a72bd7fadd96d5e1d08987f4b0abd5c1f401c682
Candy <candymdeoleo@gmail.com>
2021-12-08 15:19:00 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,90 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+
+const musicContainer = document.getElementById('media-btn');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const audio = document.getElementById('audio');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+let playImg = document.getElementById("play-img");
+let volume = document.querySelector("#volume-control"); 
+let isPlaying = false
+const songs = ['Rise', 'Hip-Hop', 'Chillpop'];
+let songIndex = 2;
+loadSong(songs[songIndex]);
+
+
+
+function loadSong(song) {
+    title.innerText = song;
+    audio.src = `audios/${song}.mp3`;
+    cover.src = `img/${song}.jpg`;
+  }
+
+
+function playSong () {
+    isPlaying = true;
+      $("#play-img").attr("src", "img/pause-thin.svg");
+      audio.play();
+        
+}
+
+function pauseSong () {
+    isPlaying = false;
+  $("#play-img").attr("src", "img/play-thin.svg");
+  
+  audio.pause();
+}
+
+function playOrPause (){
+
+    console.log('working')
+  if (isPlaying == false) {
+    return playSong();
+ }
+
+ else {
+     return pauseSong();
+ }
+
+
+}
+
+function prevSong() {
+    songIndex--;
+  
+    if (songIndex < 0) {
+      songIndex = songs.length - 1;
+    }
+  
+    loadSong(songs[songIndex]);
+  
+    playSong();
+  }
+  
+
+  function nextSong() {
+    songIndex++;
+  
+    if (songIndex > songs.length - 1) {
+      songIndex = 0;
+    }
+  
+    loadSong(songs[songIndex]);
+  
+    playSong();
+  }
+
+$("#play").on("click", playOrPause);
+$("#prev").on("click", prevSong);
+$("#next").on("click", nextSong);
+
+
+volume.addEventListener("change", function(e) {
+audio.volume = e.currentTarget.value / 100;
+})
+
+});
---
+++ readme.md
@@ -0,0 +1,13 @@
+## Music Player
+
+Simple music player that allows to play, pause and pause songs and adjust the volume.
+
+## Project Specifications
+
+Created using: 
+- HTML
+- CSS
+- Javascript
+- VSCode
+
+Made taking [Lo-fi Cafe](https://www.google.com) and [bradtraversy's music player](https://vanillawebprojects.com/projects/music-player/) as inspiration
